c87a85e300eb4bd60aff2dca5137d45c49c78654
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2024-07-18 15:36:06 +0200

    final touches

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -6,7 +6,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// normally goes to .env
+// in production goes to .env
 const secretKey = "supersecert"
 
 func GenerateToken(email, role string, userId int32) (string, error) { 
